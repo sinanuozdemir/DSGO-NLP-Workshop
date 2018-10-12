462a06ef53bf1a0dc39af2b60df7c3878f29e36a
Sinan Ozdemir <sinan.u.ozdemir@gmail.com>
2018-10-12 07:23:20 -0700

    updated server

--- server.py
+++ server.py
@@ -9,6 +9,12 @@ from flask import Flask, jsonify, request
 # initialize the Flask object
 app = Flask(__name__)
 
+known_topics = {
+    14: "refunds",
+    11: "account info",
+    12: 'cancellations'
+}
+
 @app.route('/', methods=['POST'])
 def word2vec():
     # load the pre-trained model (see notebook)
@@ -17,6 +23,7 @@ def word2vec():
     sentences = sent_tokenize(request.form["text"])
     # predict the topic identifiers using the LSA we built
     topics = map(int, loaded_lsa.predict(sentences))
+    topics = [t if t not in known_topics else known_topics[t] for t in topics]
     # return the results as a JSON
     return jsonify({"topics": topics, "sentences": sentences})
 
